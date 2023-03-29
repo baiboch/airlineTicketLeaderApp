8d2575c8198e2a6236b9ec67ecea6bce7c9d1827
Chingiz Baibogirov <c.baibogirov@platform.live>
2023-03-29 22:05:07 +0200

    initial start controller

--- src/main/java/com/airlineticketsearcher/app/AirlineTicketSearchApplication.java
+++ src/main/java/com/airlineticketsearcher/app/AirlineTicketSearchApplication.java
@@ -1,13 +1,12 @@
-package com.airlineticketsearcher.demo;
+package com.airlineticketsearcher.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AirlineTicketSearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AirlineTicketSearchApplication.class, args);
 	}
-
 }
--- src/test/java/com/airlineticketsearcher/app/DemoApplicationTests.java
+++ src/test/java/com/airlineticketsearcher/app/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.airlineticketsearcher.demo;
+package com.airlineticketsearcher.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
