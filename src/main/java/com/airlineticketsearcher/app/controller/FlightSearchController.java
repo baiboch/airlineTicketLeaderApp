8a29b1550a6899eb063e6516aa3d74fcaa57003f
chingiz <cbaibogirov@gmail.com>
2023-11-15 19:49:00 +0100

    fix search route api

---
+++ src/main/java/com/airlineticketsearcher/app/controller/FlightSearchController.java
@@ -0,0 +1,53 @@
+package com.airlineticketsearcher.app.controller;
+
+import com.airlineticketsearcher.app.model.UnifiedFlight;
+import com.airlineticketsearcher.app.model.request.UnifiedFlightSearchRequest;
+import com.airlineticketsearcher.app.service.FlightSearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "${airline_ticket_app.base_api_path}/search-flight", produces="application/json")
+@RequiredArgsConstructor
+public class FlightSearchController {
+
+    private final FlightSearchService flightSearchService;
+
+    @GetMapping(
+          value = "/",
+          consumes = "application/json",
+          produces = "application/json"
+    )
+    public ResponseEntity<String> getDirections(
+          @RequestParam(name = "from") String from,
+          @RequestParam(name = "to", required = false) String to,
+          @RequestParam(name = "maxPrice", required = false) String maxPrice) {
+
+        List<UnifiedFlight> flightList = flightSearchService.searchFlights(
+              new UnifiedFlightSearchRequest(from, to, maxPrice)
+        );
+        return ResponseEntity.ok(flightList.toString());
+    }
+
+//    @GetMapping(
+//          value = "/search",
+//          consumes = "application/json",
+//          produces = "application/json"
+//    )
+//    public ResponseEntity<String> getMaxPriceDirections(
+//          @RequestParam(name = "to") String to,
+//            @RequestParam(name = "from") String from,
+//            ) {
+//
+//        List<UnifiedFlight> flightList = flightSearchService.searchFlights(
+//              new UnifiedFlightSearchRequest(from, to, maxPrice)
+//        );
+//        return ResponseEntity.ok(flightList.toString());
+//    }
+}
